f26268f6a5fa95897dd3aa27d840a83a80d9ddaf
Geoff Smith <smith260194@gmail.com>
2019-06-04 17:58:50 +0100

    Adding database connection class

---
+++ spec/database_connection_spec.rb
@@ -0,0 +1,15 @@
+require 'database_connection'
+
+describe DatabaseConnection do
+  it '#setup saves a database connection as class instance' do
+    expect(PG).to receive(:connect).with(dbname: 'test_name')
+
+    DatabaseConnection.setup('test_name')
+  end
+
+  it 'can return an databse connection' do
+    connection = DatabaseConnection.setup('bookmark_manager_test')
+
+    expect(DatabaseConnection.connection).to eq(connection)
+  end
+end
